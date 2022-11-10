eced5e6055fa22d3b93038fc7814937796410529
darCavalier11 <sumit.pradhan65@gmail.com>
2022-11-10 22:09:40 +0530

    initialised the parse

--- lib/markdown_parser.dart
+++ lib/markdown_parser.dart
@@ -1,5 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 library markdown_parser;
 
 class MarkdownParser {
+  final String markdownText;
+  MarkdownParser({
+    required this.markdownText,
+  });
   
+
+  @override
+  String toString() => 'MarkdownParser(markdownText: $markdownText)';
+
+  @override
+  bool operator ==(covariant MarkdownParser other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.markdownText == markdownText;
+  }
+
+  @override
+  int get hashCode => markdownText.hashCode;
 }
--- lib/models/markdown_format.dart
+++ lib/models/markdown_format.dart
@@ -1,9 +1,11 @@
 import 'package:markdown_parser/models/markdown_text_styles/markdown_text_styles.dart';
 
-abstract class MarkdownElement {}
+abstract class MarkdownElement {
+
+}
 
 // markdown generic text are regular text that could be shown any where
 // like normal text, links, inline code texts.
-abstract class MarkdownGenericText extends MarkdownElement {
+abstract class MarkdownGenericText implements MarkdownElement {
   MarkdownTextStyle markdownTextStyle = MarkdownTextStyle.none;
 }
